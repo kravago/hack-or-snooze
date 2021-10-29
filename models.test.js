b72a4c2374a065f4695e60a43792d9b92edab764
Engines-screaming <karl.ravago@gmail.com>
2021-10-28 20:15:51 -0700

    added testing to help guide development

---
+++ models.test.js
@@ -0,0 +1,10 @@
+describe('Help Sending Story Data to the Backend API', function() {
+
+    beforeEach('setup method', function() {
+        // TODO: set up any methods to run tests
+    });
+
+    it('adding a new story should return a story instance', function() {
+        // TODO: write a test that can test if result is a story.
+    });
+})
